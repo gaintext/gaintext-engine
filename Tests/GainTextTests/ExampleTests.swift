75b7c5564b08e52474f4ce15116ef2be192d8b78
Martin Waitz <tali@admingilde.org>
2016-12-13 10:06:15 +0100

    Add examples from website to Tests.

---
+++ Tests/GainTextTests/ExampleTests.swift
@@ -0,0 +1,96 @@
+//
+// GainText parser
+// Copyright Martin Waitz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+
+import Engine
+import GainText
+
+import XCTest
+import Nimble
+
+
+class ExampleTests: XCTestCase {
+
+    func testWritingText() throws {
+        let doc = Document(source:
+            "Headline\n" +
+            "========\n" +
+            "\n" +
+            "Text with _embedded_ `markup`.\n" +
+            "\n" +
+            " * Layout in source is important\n" +
+            "   - allows to write beautiful source documents\n" +
+            "   - also readable by non-techies\n" +
+            " * TBD\n")
+        let nodes = doc.parse()
+
+        expect(nodes).to(haveCount(1))
+
+        expect(nodes[0].nodeType.name) == "section"
+
+        expect(nodes[0].children).to(haveCount(3))
+        expect(nodes[0].children[0].nodeType.name) == "title"
+        expect(nodes[0].children[1].nodeType.name) == "p"
+        //TBD expect(nodes[0].children[2].type.name) == "ul"
+    }
+
+    func testStructuredElements() throws {
+        let doc = Document(source:
+            "author: Martin Waitz\n" +
+            "  city: Nuremberg\n" +
+            "  country: Germany\n")
+        doc.global.register(block: ElementType("author"))
+        let nodes = doc.parse()
+
+        expect(nodes).to(haveCount(1))
+
+        expect(nodes[0].nodeType.name) == "author"
+    }
+
+    func testStructuredText() throws {
+        let doc = Document(source:
+            "title: GainText example\n" +
+            "author: Martin Waitz\n" +
+            "\n" +
+            "abstract:\n" +
+            "  This is a small example which shows some *GainText* features.\n" +
+            "\n" +
+            "Chapter 1\n" +
+            "=========\n" +
+            "\n" +
+            "Blah blah, see [figure:f1].\n" +
+            "\n" +
+            "figure: #f1\n" +
+            "  image: fig1.png\n" +
+            "  caption: A nice graphic explaining the text\n" +
+            "\n")
+        doc.global.register(block: ElementType("title"))
+        doc.global.register(block: ElementType("author"))
+        doc.global.register(block: ElementType("abstract"))
+        doc.global.register(block: ElementType("figure"))
+
+        let nodes = doc.parse()
+
+        expect(nodes).to(haveCount(4))
+
+        expect(nodes[0].nodeType.name) == "title"
+        expect(nodes[1].nodeType.name) == "author"
+        expect(nodes[2].nodeType.name) == "abstract"
+        expect(nodes[3].nodeType.name) == "section"
+
+    }
+
+    static var allTests : [(String, (ExampleTests) -> () throws -> Void)] {
+        return [
+            ("testWritingText", testWritingText),
+            ("testStructuredElements", testStructuredElements),
+            ("testStructuredText", testStructuredText),
+        ]
+    }
+}
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,20 @@
+//
+// GainText parser
+// Copyright Martin Waitz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+
 import XCTest
+
 @testable import EngineTests
 @testable import BlockTests
 @testable import MarkupTests
 //@testable import ElementsTests
+@testable import GainTextTests
 
 XCTMain([
 
@@ -33,4 +45,7 @@ XCTMain([
      // MarkupTests
      testCase(SpanWithBracketsTests.allTests),
      testCase(SpanWithDelimitersTests.allTests)
+
+     // GainTextTests
+     testCase(ExampleTests.allTests)
 ])
